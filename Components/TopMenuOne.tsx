03710a6ad607cd5dc0b070a16916508f75ec5e55
José Fong <161090897+JoseFong@users.noreply.github.com>
2026-05-26 10:39:39 -0700

    Added responsiveness for wider screens

--- Components/TopMenuOne.tsx
+++ Components/TopMenuOne.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TopMenuOne() {
   return (
-    <div className='bg-white w-full shadow-xl flex items-center justify-center fixed z-50 p-4'>
+    <div className='bg-white w-full shadow-xl max-w-3xl flex items-center justify-center fixed z-50 p-4'>
         <img
           src="https://freepngimg.com/save/62031-logo-ristretto-coffee-cafe-cup-free-transparent-image-hd/956x736"
           className='w-15'
